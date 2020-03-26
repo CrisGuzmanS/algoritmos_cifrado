fb4b4f0f443013fdf0b9b6f91fd932742e7e3bda
Cristian <cristianguzmansuarez@gmail.com>
2020-03-25 21:59:29 -0600

    Se elimino codigo de program.py para evitar confunsiones en el equipo

--- aes_cbc.py
+++ aes_cbc.py
@@ -1,3 +1,7 @@
+# ==================================
+# ENCRYPTION / DECRYPTION OPERATIONS
+# ==================================
+
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
