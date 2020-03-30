9899bfbda47804a2ac7e0513213c6c441a318f4c
Pabloerm24 <62684380+Pabloerm24@users.noreply.github.com>
2020-03-30 15:46:44 -0600

    Update rsa_pss.py

--- rsa_pss.py
+++ rsa_pss.py
@@ -22,9 +22,25 @@ class RSA_PSS:
         self.executionTime = timer() - start_time
         return signature.hex().upper()
 
-    def verify(self):
-        pass
+    def verify(self,message):
+        message = bytes.fromhex(message)    #Convierte el mensaje a bytes.
+        random_generator = Random.new().read    #Genera una secuencia aleatoria.
+        key = RSA.generate(1024,random_generator)   #Se usa la secuencia para generar un par de llaves.
+        h = SHA256.new(message) #Se obtiene la transformada hash del mensaje.
+        signature = pss.new(key).sign(h)    #Se crea un objeto que permite hacer la firma y validación.
+        start_time = timer()    #Se empieza a tomar el tiempo de ejecución.
+        verifier = pss.new(key.publickey()) #Se crea el objeto capaz de validar la firma, para ello se usa la llave pública.
+        try:
+            verifier.verify(h,signature)    #Función que hace la valdiación de la firma.
+            self.executionTime = timer() - start_time   #Fin de la toma de tiempo.
+            print("La firma es correcta.")
+            return 1    #La firma es auténtica.
+        except(ValueError, TypeError):
+            self.executionTime = timer() - start_time   #fin de la toma de tiempo.
+            print("La firma no es correcta.")
+            return 0    #La firma no es auténtica.
 
 message = "04df2328374635f9f023"
 
 rsa_pss = RSA_PSS()
+rsa_pss.verify(message)
