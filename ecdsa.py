5309b1807c1f35f4079dbe469872e3f1e718d17c
Pabloerm24 <62684380+Pabloerm24@users.noreply.github.com>
2020-03-30 15:41:24 -0600

    Update ecdsa.py

--- ecdsa.py
+++ ecdsa.py
@@ -14,8 +14,27 @@ class ECDSA:
         signature = signer.sign(h)
         self.executionTime = timer() - start_time
         return signature
+    
+    def verify(self,message):
+    	key = ECC.generate(curve='P-521')	#Genera una nueva llave privada usando la curva definida por P-521
+    	h = SHA512.new(message)		#Genera la función Hash del mensaje.
+    	signer = DSS.new(key, 'fips-186-3')	#Crea un objeto signature para hacer la firma.
+    	signature = signer.sign(h)	#Genera la firma del mensaje.
+    	pubKey = key.public_key()	#Obtiene la llave pública del par generado almacenado en "key".
+    	start_time = timer()	#Función que permite iniciar una toma de tiempo.
+    	verifier = DSS.new(pubKey, 'fips-186-3')	#Crea un objeto signature para hacer la validación de la firma.
+    	try:
+    		verifier.verify(h,signature)	#Hace la comparación entre el hash del mensaje y la firma.
+    		print("La firma se pudo validar, es correcta.")
+    		self.executionTime = timer() - start_time	#Fin de la toma de tiempo.
+    		return 1
+    	except(ValueError):	#Accede en caso de que los valores no sean correctos, es decir, la firma no es válida.
+    		print("La firma no se pudo validar, no es correcta")
+    		self.executionTime = timer() - start_time	#Fin de la toma de tiempo.
+    		return 0
 
 message = 'I give my permission to order #4355'
 
 ecdsa = ECDSA()
 ecdsa.sign(message)
+ecdsa.verify(message)
